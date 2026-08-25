ae28e74cf0028db01d117110d3e3709ca7745dd5
rystei <gustavo208cardoso@gmail.com>
2026-08-25 20:36:51 -0300

    switch

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -23,6 +23,31 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("3. Avaliar uma banda");
     Console.WriteLine("4. Visualizar a média de uma banda");
     Console.WriteLine("5. Para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    String opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: 
+            Console.WriteLine("Opção escolhida foi a " + opcaoEscolhidaNumerica);
+            break;
+        case 2: 
+            Console.WriteLine("Opção escolhida foi a " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Opção escolhida foi a " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Opção escolhida foi a " + opcaoEscolhidaNumerica);
+            break;
+        case 5:
+            Console.WriteLine("Opção escolhida foi a " + opcaoEscolhidaNumerica);
+            break;
+        default : Console.WriteLine("Opção inválida, digite novamente");
+            break;
+    }
 }
 
 ExibirMensagemDeBoasVindas();
