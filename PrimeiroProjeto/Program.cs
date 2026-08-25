c8267481814f88d4a58dce22285f8bbca73d6949
rystei <gustavo208cardoso@gmail.com>
2026-08-25 20:10:37 -0300

    Variaveis do tipo texto

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -4,12 +4,29 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound! O seu app de streami
 
 void ExibirMensagemDeBoasVindas()
 {
-    Console.WriteLine("************************************");
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███╗░░░███╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝████╗░████║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░██╔████╔██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██║╚██╔╝██║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗██║░╚═╝░██║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("************************************");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nMenu Principal:");
+    Console.WriteLine("1. Registrar uma banda");
+    Console.WriteLine("2. Exibir todas as bandas");
+    Console.WriteLine("3. Avaliar uma banda");
+    Console.WriteLine("4. Visualizar a média de uma banda");
+    Console.WriteLine("5. Para sair");
 }
 
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
 
 
 
