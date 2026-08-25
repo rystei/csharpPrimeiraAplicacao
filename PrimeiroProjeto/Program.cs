9bf4334777c46262552f531c494b670bc7befbe3
rystei <gustavo208cardoso@gmail.com>
2026-08-25 19:55:48 -0300

    Exemplo função

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -2,4 +2,14 @@
 
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound! O seu app de streaming de música";
 
-Console.WriteLine(mensagemDeBoasVindas);
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("************************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("************************************");
+}
+
+ExibirMensagemDeBoasVindas();
+
+
+
