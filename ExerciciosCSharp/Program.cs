54f0fb2d2082ee7b14d41060e4b2f8206446f0f9
rystei <gustavo208cardoso@gmail.com>
2026-08-27 21:50:56 -0300

    Exercicio numero secreto

--- ExerciciosCSharp/Program.cs
+++ ExerciciosCSharp/Program.cs
@@ -1,6 +1,26 @@
-﻿String[] jogos = { "GTA", "FIFA", "MARIO", "MINECRAFT" };
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 100);
 
-Console.WriteLine("Digite o nome do jogo que deseja buscar: "); 
-int posicaoLista = int.Parse(Console.ReadLine()!);
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(jogos[posicaoLista]);
+    {
+        if (chute == numeroSecreto)
+        {
+            Console.WriteLine("Acertou");
+            break;
+        }
+        else if (chute > numeroSecreto)
+        {
+            Console.WriteLine("O chute é maior que o numero secreto");
+        }
+        else
+        {
+            Console.WriteLine("O chute é menor que o numero secreto");
+        }
+
+    }
+
+} while (true);
