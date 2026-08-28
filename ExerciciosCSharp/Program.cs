c94a8e013582cb611ce3c9d25a6297543ecf6f78
rystei <gustavo208cardoso@gmail.com>
2026-08-27 21:27:36 -0300

    Lista exercicio

---
+++ ExerciciosCSharp/Program.cs
@@ -0,0 +1,6 @@
+﻿String[] jogos = { "GTA", "FIFA", "MARIO", "MINECRAFT" };
+
+Console.WriteLine("Digite o nome do jogo que deseja buscar: "); 
+int posicaoLista = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(jogos[posicaoLista]);
