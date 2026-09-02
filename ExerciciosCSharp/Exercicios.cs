3ced994a6cb0e80a20c68e871377e01b1aef49f7
rystei <gustavo208cardoso@gmail.com>
2026-09-01 21:20:54 -0300

    Exercicios: Operações matematicas

---
+++ ExerciciosCSharp/Exercicios.cs
@@ -0,0 +1,17 @@
+﻿void ExibirOperacoes()
+{
+    float a = 3.15f;
+    float b = 4.50f;
+
+    float soma = a + b;
+    float subtracao = a - b;
+    float multiplicacao = a * b;
+    float divisao = a / b;
+
+    Console.WriteLine($"Soma {soma} ");
+    Console.WriteLine($"subtração {subtracao} ");
+    Console.WriteLine($"multiplicação {multiplicacao} ");
+    Console.WriteLine($"divisão {divisao} ");
+}
+
+ExibirOperacoes();
