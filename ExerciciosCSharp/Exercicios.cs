7f8b5eacf14c09decf6240ae841699b7dc074b93
rystei <gustavo208cardoso@gmail.com>
2026-09-14 20:54:05 -0300

    Criar um programa que simule um sistema de login utilizando um dicionário para armazenar nomes de usuário e senhas.

--- ExerciciosCSharp/Exercicios.cs
+++ ExerciciosCSharp/Exercicios.cs
@@ -1,29 +1,21 @@
-﻿//Crie um programa que implemente um quiz simples de perguntas e respostas. Utilize um dicionário para armazenar as perguntas e as respostas corretas.
+﻿//Criar um programa que simule um sistema de login utilizando um dicionário para armazenar nomes de usuário e senhas.
 
-Dictionary<string, string> quiz = new Dictionary<string, string>
+Dictionary<string, string> login =  new Dictionary<string, string>
 {
-    {"Qual a cor do mar?", "Azul" },
-    {"Qual a cor do café?", "Preto" },
-    {"Quanto é 10 * 2?", "20" }
+    {"Telles", "123" },
+    {"Admin", "admin" }
 };
 
-int pontucao = 0;
+Console.Write("Digite o login: ");
+string usuario = Console.ReadLine()!;
+Console.Write("Digite a senha: ");
+string senha = Console.ReadLine()!;
 
-foreach (var item in quiz)
+if (login.ContainsKey(usuario) && login[usuario] == senha)
 {
-    Console.WriteLine(item.Key);
-    Console.Write("Insira a resposta: ");
-    string resposta = Console.ReadLine()!;
-
-    if (resposta.ToLower() == item.Value.ToLower())
-    {
-        Console.WriteLine("Resposta correta!");
-        pontucao++;
-    }
-    else
-    {
-        Console.WriteLine($"Errado, a resposta correta é {item.Value}\n");
-    }
+    Console.WriteLine("Login bem-sucedido!");
 }
-
-Console.WriteLine($"Pontuação final do jogo {pontucao}/{quiz.Count} perguntas.");
+else
+{
+    Console.WriteLine("Nome de usuário ou senha incorretos.");
+}
